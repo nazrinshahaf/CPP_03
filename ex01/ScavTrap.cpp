7f069cb1776303b1b70175898eb60dba652b9757
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-06-13 17:51:20 +0800

    Added Copy constructor for scavtrap

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:05:27 by nfernand          #+#    #+#             */
-/*   Updated: 2022/06/13 16:58:08 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/06/13 17:41:48 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,31 @@ ScavTrap::ScavTrap(string name): ClapTrap(name)
 	cout << "Allow me to introduce myself - I am a" BLUE " <SC4V-TP> " RESET
 		"steward bot, but my friends call me " YELLOW "<"
 		<< name << ">" RESET "." << endl;
-	setName(name);
 	setHealthPoints(100);
 	setEnergyPoints(50);
 	setAttackDamage(20);
 	this->_guard_gate_mode = 0;
 	cout << endl;
+}
+
+ScavTrap::ScavTrap(ScavTrap const &tocopy) : ClapTrap(tocopy.getName())
+{
+	cout << GREEN "ScavTrap Copy Constructor Called" RESET << endl;
+	setHealthPoints(tocopy.getHealthPoints());
+	setEnergyPoints(tocopy.getEnergyPoints());
+	setAttackDamage(tocopy.getAttackDamage());
+	this->_guard_gate_mode = tocopy.getGuardGateMode();
+}
+
+ScavTrap	&ScavTrap::operator=(ScavTrap const &tocopy)
+{
+	cout << GREEN "ScavTrap Copy Assignment Operator Called" RESET << endl;
+	setName(tocopy.getName());
+	setHealthPoints(tocopy.getHealthPoints());
+	setEnergyPoints(tocopy.getEnergyPoints());
+	setAttackDamage(tocopy.getAttackDamage());
+	this->_guard_gate_mode = tocopy.getGuardGateMode();
+	return (*this);
 }
 
 ScavTrap::~ScavTrap()
@@ -43,4 +62,14 @@ void	ScavTrap::guardGate(void)
 		cout << MAGENTA "Activating guard gate mode..." RESET << endl;
 	else
 		cout << MAGENTA "Deactivating guard gate mode..." RESET << endl;
+}
+
+bool	ScavTrap::getGuardGateMode(void) const
+{
+	return (this->_guard_gate_mode);
+}
+
+void	ScavTrap::setGuardGateMode(bool value)
+{
+	this->_guard_gate_mode = value;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 10:34:59 by nfernand          #+#    #+#             */
-/*   Updated: 2022/06/13 17:33:11 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/06/13 17:50:20 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,63 @@ using std::endl;
 
 int	main()
 {
-	ScavTrap	scav_trap("Cluck-Trap");
+	cout << CYAN "[Testing ScavTrap main functions]" RESET << endl;
+	{
+		ScavTrap	scav_trap("Cluck-Trap");
 
-	scav_trap.attack("CLAP-9000");
-	scav_trap.guardGate();
-	scav_trap.guardGate();
+		scav_trap.attack("CLAP-9000");
+		scav_trap.guardGate();
+		scav_trap.guardGate();
+	}
+	cout << endl;
+
+	cout << CYAN "[Testing Copy Constructor and Copy Assignment Operator]" RESET << endl;
+	{
+		cout << CYAN "[Testing Copy Constructor]" RESET << endl;
+		{
+			ScavTrap	*scav_trap = new ScavTrap("Cluck-Trap");
+
+			cout << MAGENTA "Changing values for original..." RESET << endl;
+			scav_trap->setName("some bullshit name here");
+			cout << endl;
+
+			cout << MAGENTA "Copying ScavTrap..." RESET << endl;
+			ScavTrap	*copy_trap = new ScavTrap(*scav_trap);
+			cout << endl;
+
+			cout << MAGENTA "Printing values for ScavTrap..." RESET << endl;
+			cout << "Copy name: " << copy_trap->getName() << endl;
+			cout << endl;
+
+			delete copy_trap;
+			delete scav_trap;
+		}
+		cout << endl;
+
+		cout << CYAN "[Copy Assignment Operator]" RESET << endl;
+		{
+			ScavTrap	*scav_trap = new ScavTrap("Cluck-Trap");
+
+			cout << MAGENTA "Changing values for original..." RESET << endl;
+			scav_trap->setName("some bullshit name here");
+			cout << endl;
+
+			cout << MAGENTA "Creating CopyTrap..." RESET << endl;
+			ScavTrap	*copy_trap = new ScavTrap("Copy-Trap");
+			cout << endl;
+
+			cout << MAGENTA "Printing values for CopyTrap..." RESET << endl;
+			cout << "Copy name: " << copy_trap->getName() << endl;
+			cout << endl;
+
+			*copy_trap = *scav_trap;
+
+			cout << MAGENTA "Printing values for CopyTrap..." RESET << endl;
+			cout << "Copy name: " << copy_trap->getName() << endl;
+			cout << endl;
+
+			delete copy_trap;
+			delete scav_trap;
+		}
+	}
 }
